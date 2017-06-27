87e08525c8a396fb40f5c23fd6a1030930acf020
vaberay <ramon.e.savinon@gmail.com>
2017-06-27 00:20:48 -0700

    Add basic page and routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Amazon Web Scraping' });
+});
+
+
+// POST form data
+router.post('/', function(req, res, next){
+  res.render('index', { title: 'Amazon Web Scraping' });
 });
 
 module.exports = router;
