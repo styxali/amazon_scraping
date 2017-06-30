b95762b0724caeb79ec7976d994cb3ef67be23b2
vaberay <ramon.e.savinon@gmail.com>
2017-06-30 00:46:38 -0700

    Use Redis to store and filter data

--- routes/index.js
+++ routes/index.js
@@ -1,47 +1,51 @@
 var express = require('express');
 var router = express.Router();
+var redis = require('redis')
+var redisClient = redis.createClient()
 
 var axios = require('axios');
 var cheerio = require('cheerio');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Amazon Web Scraping' });
+  redisClient.sort('ramon', 'LIMIT', 0, 25, 'get', '*->title', 'get',
+    '*->reviews', 'get', '*->rating', 'get', '*->time', function (err, reply) {
+      var newArray = []
+      while (reply.length) newArray.push(reply.splice(0, 4));
+      res.render('index', { productsInfo: newArray });
+    }
+  )
 });
-
 
 // POST form data
 router.post('/', function(req, res, next){
   var asin = req.body.asin;
-  var url = `http://www.amazon.com/dp/${asin}`
+  console.warn('asin', typeof(asin))
+  if (!asin) {
+    res.redirect('/');
+  }
+
+  var url = `http://www.amazon.ca/dp/${asin}`
 
   axios.get(url).then( (response) => {
     var $ = cheerio.load(response.data, {ignoreWhiteSpace: true });
     var info = {
-      title: $('#productTitle').text().trim()
+      title: $('#productTitle').text().trim(),
+      reviews: $('#acrCustomerReviewText').first().text(),
+      rating: $('i.a-icon.a-icon-star').children().first().text()
     }
-      // kurs.push( {
-      //   currency: $(elm).children().first().text(),
-      //   erate: {
-      //     sell: $(elm).children().eq(1).first().text(),
-      //     buy: $(elm).children().eq(2).first().text()
-      //   },
-      //   tt: {
-      //     sell: $(elm).children().eq(3).first().text(),
-      //     buy: $(elm).children().eq(4).first().text()
-      //   },
-      //   notes: {
-      //     sell: $(elm).children().eq(5).first().text(),
-      //     buy: $(elm).children().eq(6).first().text()
-      //   }
-      // });
-    // });
+
     return(info);
-  })
-  .then ( (kurs) => {
-    res.send(kurs);
+  }).then ( (info) => {
+    var ts = Math.round((new Date()).getTime() / 1000);
+
+    redisClient.zadd('ramon', ts, asin, function(err, reply) {
+      redisClient.hmset(asin, ['title', info.title, 'reviews', info.reviews, 'rating', info.rating, 'time', ts])
+    });
+
+    res.redirect('/');
   }).catch ( (error) => {
-    res.send(error);
+    res.redirect('/');
   })
 });
 
