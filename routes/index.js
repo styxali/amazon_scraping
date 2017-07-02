f5bd6744bfdce58c93fba0a207e0bbdf20d3572a
vaberay <ramon.e.savinon@gmail.com>
2017-07-02 07:36:53 -0700

    use cookies for client identification

--- routes/index.js
+++ routes/index.js
@@ -5,22 +5,37 @@ var redisClient = redis.createClient()
 
 var axios = require('axios');
 var cheerio = require('cheerio');
+var randomHex = require('random-hexadecimal');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  redisClient.sort('ramon', 'by', '*->time', 'LIMIT', 0, 25, 'desc', 'get', '*->title', 'get',
-    '*->reviews', 'get', '*->rating', 'get', '*->time', function (err, reply) {
-      var newArray = []
-      while (reply.length) newArray.push(reply.splice(0, 4));
-      res.render('index', { productsInfo: newArray });
-    }
-  )
+  var id = req.cookies['canopy-aws']
+  var newArray = []
+
+  if (!!id) {
+    redisClient.expire(id, 604800)
+    redisClient.sort(id, 'by', '*->time', 'LIMIT', 0, 25, 'desc', 'get', '*->title', 'get',
+      '*->reviews', 'get', '*->rating', 'get', '*->time', function (err, reply) {
+        while (reply.length) newArray.push(reply.splice(0, 4));
+        res.render('index', { productsInfo: newArray });
+      }
+    )
+  } else {
+    res.render('index', { productsInfo: newArray });
+  }
 });
 
 // POST form data
 router.post('/', function(req, res, next){
+  var id = req.cookies['canopy-aws']
+
+  if (!id) {
+    // create the hex and set the cookie
+    id = randomHex({ max: 200 })
+    res.cookie('canopy-aws', id)
+  }
+
   var asin = req.body.asin;
-  console.warn('asin', typeof(asin))
   if (!asin) {
     res.redirect('/');
   }
@@ -41,7 +56,7 @@ router.post('/', function(req, res, next){
     var productInfo = ['title', info.title, 'reviews', info.reviews, 'rating', info.rating, 'time', ts]
 
     redisClient.zadd('allSearches', ts, asin)
-    redisClient.zadd('ramon', ts, asin, function(err, reply) {
+    redisClient.zadd(id, ts, asin, function(err, reply) {
       redisClient.hmset(asin, productInfo)
     });
 
